7e63d517f50c89ae62324ff9880dbb2b4f8138a2
EMigliaretti <elisabetta.migliaretti@gminformatica.it>
2025-04-23 16:40:44 +0200

    Update programma_crivello.py

--- programma_crivello.py
+++ programma_crivello.py
@@ -1,7 +1,9 @@
+# Esercizio proposto e risolto in riunione
+
 # Numeri primi fino a "n"
 
 def crivello(n):
-    l = set(range(2, n+1))
+    l = set(range(2, n + 1))
     for i in range(2, int(n**0.5) + 1):
         l = l.difference({i * x for x in range(2, n + 1)})
     return l
